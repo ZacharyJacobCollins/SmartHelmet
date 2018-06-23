7bb50dd3f3be1837e8bc9860f5b47ad1aa6b58d6
ZacharyJacobCollins <zacharyjacobcollins@gmail.com>
2018-06-23 19:00:50 -0400

    update response

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-response_entity = { "conversationToken":"[]", "expectUserResponse":true, "expectedInputs":[ { "inputPrompt":{ "richInitialPrompt":{ "items":[ { "simpleResponse":{ "textToSpeech":"WelcometoPass" } } ] } }, "possibleIntents":[ { "intent":"assistant.intent.action.TEXT" } ] } ], "responseMetadata":{ "status":{ "message":"Success(200)" }, "queryMatchInfo":{ "queryMatched":true, "intent":"2379c8dd-f563-40ca-8da2-401cb2f2a2bb" } } }
+response_entity = '{ "conversationToken": "", "expectUserResponse": true, "expectedInputs": [ { "inputPrompt": { "richInitialPrompt": { "items": [ { "simpleResponse": { "textToSpeech": "Math and prime numbers it is!" } }, { "basicCard": { "title": "Math & prime numbers", "formattedText": "42 is an even composite number. It\n is composed of three distinct prime numbers multiplied together. It\n has a total of eight divisors. 42 is an abundant number, because the\n sum of its proper divisors 54 is greater than itself. To count from\n 1 to 42 would take you about twenty-one…", "image": { "url": "https://example.google.com/42.png", "accessibilityText": "Image alternate text" }, "buttons": [ { "title": "Read more", "openUrlAction": { "url": "https://example.google.com/mathandprimes" } } ], "imageDisplayOptions": "CROPPED" } } ], "suggestions": [] } }, "possibleIntents": [ { "intent": "actions.intent.TEXT" } ] } ] }'
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -17,11 +17,7 @@ def dashboard():
         }
         return render_template('dashboard.html', **templateData)
     else: 
-<<<<<<< HEAD
         return response_entity
-=======
-        return ''
->>>>>>> 2466c2c4388a2385eed756c8484a30c6a93845cf
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
