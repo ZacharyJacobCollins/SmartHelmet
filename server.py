2f6654b3f4acb35868f79caa3ecf3e146ae9dae0
Zac <zacharyjacobcollins@gmail.com>
2018-06-23 22:59:07 +0000

    updates to view

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-response_entity = { "conversationToken":"[]", "expectUserResponse":true, "expectedInputs":[ { "inputPrompt":{ "richInitialPrompt":{ "items":[ { "simpleResponse":{ "textToSpeech":"WelcometoPass" } } ] } }, "possibleIntents":[ { "intent":"assistant.intent.action.TEXT" } ] } ], "responseMetadata":{ "status":{ "message":"Success(200)" }, "queryMatchInfo":{ "queryMatched":true, "intent":"2379c8dd-f563-40ca-8da2-401cb2f2a2bb" } } }
+response_entity = '{ "conversationToken":"[]", "expectUserResponse":true, "expectedInputs":[ { "inputPrompt":{ "richInitialPrompt":{ "items":[ { "simpleResponse":{ "textToSpeech":"WelcometoPass" } } ] } }, "possibleIntents":[ { "intent":"assistant.intent.action.TEXT" } ] } ], "responseMetadata":{ "status":{ "message":"Success(200)" }, "queryMatchInfo":{ "queryMatched":true, "intent":"2379c8dd-f563-40ca-8da2-401cb2f2a2bb" } } }'
+
+response_message = '["messages":[{"speech":"Text response","type":0}]]'
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -17,11 +19,7 @@ def dashboard():
         }
         return render_template('dashboard.html', **templateData)
     else: 
-<<<<<<< HEAD
-        return response_entity
-=======
-        return ''
->>>>>>> 2466c2c4388a2385eed756c8484a30c6a93845cf
+        return (response_message)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
