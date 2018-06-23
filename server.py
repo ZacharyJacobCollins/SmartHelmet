affcf07ab0cd2afc0d6cf41e0679ebcdf0a84d69
ZacharyJacobCollins <zacharyjacobcollins@gmail.com>
2018-06-23 19:07:30 -0400

    add all

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-response_entity = '{ "conversationToken": "", "expectUserResponse": true, "expectedInputs": [ { "inputPrompt": { "richInitialPrompt": { "items": [ { "simpleResponse": { "textToSpeech": "Math and prime numbers it is!" } }, { "basicCard": { "title": "Math & prime numbers", "formattedText": "42 is an even composite number. It\n is composed of three distinct prime numbers multiplied together. It\n has a total of eight divisors. 42 is an abundant number, because the\n sum of its proper divisors 54 is greater than itself. To count from\n 1 to 42 would take you about twenty-one…", "image": { "url": "https://example.google.com/42.png", "accessibilityText": "Image alternate text" }, "buttons": [ { "title": "Read more", "openUrlAction": { "url": "https://example.google.com/mathandprimes" } } ], "imageDisplayOptions": "CROPPED" } } ], "suggestions": [] } }, "possibleIntents": [ { "intent": "actions.intent.TEXT" } ] } ] }'
+response_entity = '{"conversationToken":"","expectUserResponse":true,"expectedInputs":[{"inputPrompt":{"richInitialPrompt":{"items":[{"simpleResponse":{"textToSpeech":"Mathandprimenumbersitis!"}},{"basicCard":{"title":"Math&primenumbers","formattedText":"42isanevencompositenumber.It\niscomposedofthreedistinctprimenumbersmultipliedtogether.It\nhasatotalofeightdivisors.42isanabundantnumber,becausethe\nsumofitsproperdivisors54isgreaterthanitself.Tocountfrom\n1to42wouldtakeyouabouttwenty-one…","image":{"url":"https://example.google.com/42.png","accessibilityText":"Imagealternatetext"},"buttons":[{"title":"Readmore","openUrlAction":{"url":"https://example.google.com/mathandprimes"}}],"imageDisplayOptions":"CROPPED"}}],"suggestions":[]}},"possibleIntents":[{"intent":"actions.intent.TEXT"}]}]}'
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -17,7 +17,7 @@ def dashboard():
         }
         return render_template('dashboard.html', **templateData)
     else: 
-        return response_entity
+        return jsonify(response_entity)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
